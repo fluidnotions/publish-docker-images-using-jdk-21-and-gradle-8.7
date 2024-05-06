196f905f553e4b90e343b3afa655cc58eb544a0a
Justin Robinson <justin.robinson@vcontractor.co.za>
2024-05-06 16:47:07 +0200

    bug fixes

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 ## Build The Runner
 
-* docker build -t dockerize-boot-services:1.0.0 .
+```bash
+docker build -t cybicom/dockerize-boot-services:1.0.0 .
+```
+
+## Publish The Runner
+
+```bash
+docker build -t cybicom/dockerize-boot-services:1.0.0 .
+```
 
 ## About 
 
@@ -23,25 +31,34 @@ docker pull cybicom/dockerize-boot-services:1.0.0
 Use the following command to run the container:
 
 ```bash
-docker run -it -v %PROJECTS%:/app/projects -v /var/run/docker.sock:/var/run/docker.sock -e CLI dockerize-boot-services:1.0.0
+docker run -it -v %PROJECTS%:/app/projects -v /var/run/docker.sock:/var/run/docker.sock -e CLI=true dockerize-boot-services:1.0.0
 ```
 
 ### Volume Mounts
 
 - **Projects Directory**: Mount your local projects directory to `/app/projects` in the container. Replace `%PROJECTS%` with the path to your projects directory on your host.
   
-- **Docker Socket**: Mounts the Docker socket to allow the container to communicate with the host Docker daemon, enabling it to push new images the host's Docker registry.
+- **Docker Socket**: Mounts the Docker socket to allow the container to communicate with the host Docker daemon, enabling it to push new images to the host's Docker registry.
 
 ### Environment Variables
 
-- **CLI**: Set this environment variable to enable the command-line interface features within the container. When enabled, you can choose specific repositories to build and specify a tag version. If not enabled, all repositories are built and published with the 'latest' tag.
+- **CLI**: Set this environment variable to `true` to enable the command-line interface features within the container. When enabled, you can choose specific repositories to build and specify a tag version. If not enabled, all repositories are built and published with the 'latest' tag.
+
+- **PROJECTS_DIR**: Optionally set this to specify a custom directory for Spring Boot projects within the container. Defaults to `/app/projects`.
+
+- **PROJECT_PATH**: Specify a single project path if you want to build just one project. This is typically used with automation or scripts that target specific projects.
+
+- **DOCKER_TAG**: Use this to define a custom tag for the built Docker images. Defaults to 'latest' if not specified.
 
 ### Usage
 
-After running the container, it will either:
-- **CLI Enabled**: Allow you to select specific Spring Boot projects to build, and to specify a tag version for each.
-- **CLI Disabled**: Automatically build and publish all detected Spring Boot projects using JDK 21 and Gradle 8.7 with the tag 'latest'.
+After running the container, the behavior depends on the provided environment variables:
+- **CLI Enabled**: Allows you to select specific Spring Boot projects to build and to specify a tag version for each.
+- **CLI Disabled**: Automatically builds and publishes all detected Spring Boot projects using JDK 21 and Gradle 8.7 with the tag 'latest'.
+- **PROJECT_PATH Specified**: If the `-e PROJECT_PATH` is provided, the container will build only the specified project. This is useful for targeted builds, such as in automated pipelines or for specific deployment tasks.
 
 This process simplifies the deployment of multiple Spring Boot applications by automating their containerization.
 
+For more information and updates, visit either     
 [Docker Hub](https://hub.docker.com/repository/docker/cybicom/dockerize-boot-services/general)
+[Github Repo](https://github.com/fluidnotions/publish-docker-images-using-jdk-21-and-gradle-8.7)
--- dockerize-boot-services.mjs
+++ dockerize-boot-services.mjs
@@ -2,45 +2,66 @@ import { input } from "@inquirer/prompts";
 import select from "@inquirer/select";
 import checkbox from "@inquirer/checkbox";
 import { readdirSync, existsSync } from "fs";
-import { spawn } from "child_process";
+import { exec } from "child_process";
 import { join } from "path";
 
+const execPromise = (command, projectDir) => {
+  return new Promise((resolve, reject) => {
+    const process = exec(command, { cwd: projectDir });
+    process.stdout.on("data", (data) => {
+      console.log(data);
+    });
+    process.stderr.on("data", (data) => {
+      console.error(data);
+    });
+    process.on("exit", (code) => {
+      if (code === 0) {
+        console.log("Docker image built successfully.");
+        resolve();
+      } else {
+        reject(new Error(`Process exited with code ${code}`));
+      }
+    });
+    process.on("error", (err) => {
+      reject(err);
+    });
+  });
+};
 
-const runCommand = async (command, args, cwd) => {
+const removeLocks = async (projectDir) => {
+  console.log("Removing lock files...");
   try {
-      await new Promise((resolve, reject) => {
-          const child = spawn(command, args, {
-              stdio: 'inherit',
-              shell: true,
-              cwd
-
-          });
-          child.on('error', reject);
-          child.on('close', (code) => {
-              if (code === 0) {
-                  resolve();
-              } else {
-                  reject(new Error(`Command exited with code ${code}`));
-              }
-          });
-      });
-      console.log('Command completed successfully.');
+    await execPromise(
+      `find ./.gradle -type f -name "*.lock" -exec rm {} +`,
+      projectDir
+    );
+    console.log("Lock files removed.");
   } catch (err) {
-      console.error('Command failed:', err);
+    console.error("Failed to Removing lock files:", err);
   }
 };
 
 const bootBuildImageWithGradle = async (projectDir, tag) => {
-  console.log(`img building ${projectDir}`);
-  await runCommand('gradle', ['bootBuildImage', `-PimageTag=${tag}`], projectDir)
-  console.log(`img built ${projectDir}`)
+  await removeLocks(projectDir);
+  console.log(`process.env.GRADLE_HOME: ${process.env.GRADLE_HOME}`);
+  const gradleBin = process.env.GRADLE_HOME
+    ? `${process.env.GRADLE_HOME}/bin/gradle`
+    : "gradle";
+  console.log(
+    `Building Docker image for project in directory: ${projectDir}, with gradleBin: ${gradleBin}`
+  );
+  const command = `${gradleBin} bootBuildImage -PimageTag=${tag}`;
+  try {
+    await execPromise(command, projectDir);
+  } catch (err) {
+    console.error("Failed to build Docker image:", err);
+  }
 };
 
 const selectAndDockerize = async (envDir, headless = false) => {
- 
-  const directory = (envDir && envDir.trim()) || '/app/projects';
+  const directory = (envDir && envDir.trim()) || "/app/projects";
   let dockerTag = "latest";
-  if(!headless){
+  if (!headless) {
     const check = await select({
       message:
         "Have you updated all repo branches to the latest version, for your env branch of the microservices?\nHave you disconnected from the VPN?",
@@ -60,17 +81,14 @@ const selectAndDockerize = async (envDir, headless = false) => {
       default: "latest",
     });
   }
- 
 
   const files = readdirSync(directory).filter((f) => {
-    const hasGradleSub = existsSync(
-      join(directory, f, "gradle")
-    );
+    const hasGradleSub = existsSync(join(directory, f, "gradle"));
     const isNotHidden = !f.startsWith(".");
     return hasGradleSub && isNotHidden;
   });
-  let include = files
-  if(!headless){
+  let include = files;
+  if (!headless) {
     include = await checkbox({
       message: "Select microservices to dockerize",
       choices: files.map((f) => {
@@ -79,13 +97,30 @@ const selectAndDockerize = async (envDir, headless = false) => {
     });
   }
   console.log("selected microservices", include, " building docker images");
-  await Promise.all(include.map((f) => {
-    const projectDir = join(directory, f);
-    return bootBuildImageWithGradle(projectDir, dockerTag);
-  }));
+  const concurrent = process.env.CONCURRENT;
+  if (concurrent) {
+    Promise.all(
+      include.map((f) => {
+        const projectDir = join(directory, f);
+        return bootBuildImageWithGradle(projectDir, dockerTag);
+      })
+    );
+  } else {
+    for (const f of include) {
+      const projectDir = join(directory, f);
+      await bootBuildImageWithGradle(projectDir, dockerTag);
+    }
+  }
+
   console.log("finished building selected docker images");
 };
 
-const envDir = process.env.PROJECTS_DIR
-const headless = !!process.env.CLI
-await selectAndDockerize(envDir, headless);
+const envDir = process.env.PROJECTS_DIR;
+const headless = !!process.env.CLI;
+const projectPath = process.env.PROJECT_PATH;
+const dockerTag = process.env.DOCKER_TAG;
+if (!projectPath) {
+  await selectAndDockerize(envDir, headless);
+} else {
+  await bootBuildImageWithGradle(projectPath, dockerTag);
+}
